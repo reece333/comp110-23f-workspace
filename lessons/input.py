c522ea77445f4abfc659854c40d3c3d14b168dff
reece corter <rcorter@unc.edu>
2023-08-25 17:18:03 -0400

    lesson 4

---
+++ lessons/input.py
@@ -0,0 +1,5 @@
+"""Demonstrates asking the user for input"""
+
+user_name: str = input("What is your name? ")
+print("Hello!! " + user_name + ", good morning!!!!!!!")
+print("wow so cool wow, " + user_name)
