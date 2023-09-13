324fdaaa7073d7389b43196ecd4d7da86f20b29f
reece corter <rcorter@unc.edu>
2023-09-13 15:21:40 -0400

    EX 01 progress

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,42 @@
+"""EX01 - Chardle"""
+__author__ = "730564969"
+
+guess: str = input("Enter a 5-character word: ")
+if len(guess) != 5: 
+    print("Error: Word must contain 5 characters")
+    exit()
+character: str = input("Enter a single character: ")
+if len(character) != 1:
+    print("Error: Character must be a single character")
+    exit()
+instances: int = 0
+
+print("Searching for " + character + " in " + guess)
+if character == guess[0] :
+    print(character + " found in index 0")
+    instances = instances + 1
+if character == guess[1]:
+    print(character + " found in index 1")
+    instances = instances + 1
+if character == guess[2]:
+    print(character + " found in index 2")
+    instances = instances + 1
+if character == guess[3]:
+    print(character + " found in index 3")
+    instances = instances + 1
+if character == guess[4]:
+    print(character + " found in index 4")
+    instances = instances + 1
+
+if instances == 0:
+    print("No instances of " + character + " found in " + guess)
+elif instances == 1:
+    print("1 instance of " + character + " found in " + guess)
+elif instances == 2:
+    print("2 instances of " + character + " found in " + guess)
+elif instances == 3:
+    print("3 instances of " + character + " found in " + guess)
+elif instances == 4:
+    print("4 instances of " + character + " found in " + guess)
+elif instances == 5:
+    print("5 instances of " + character + " found in " + guess)
