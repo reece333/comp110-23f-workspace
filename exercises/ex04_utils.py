0fab090371f6b3cf60970b158370055f7a2396b3
reece corter <rcorter@unc.edu>
2023-10-11 14:52:39 -0400

    ex04 100

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,7 +1,9 @@
 """Exercise 4 - List Utility Functions."""
 __author__ = "730564969"
 
-def all(int_list, num) -> bool:
+
+def all(int_list: list[int], num: int) -> bool:
+    """Check if all ints in list are the same."""
     if len(int_list) == 0:
         return False
 
@@ -12,7 +14,9 @@ def all(int_list, num) -> bool:
         index += 1
     return True
 
+
 def max(input: list[int]) -> int:
+    """Find max value in given list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
 
@@ -29,7 +33,9 @@ def max(input: list[int]) -> int:
 
     return max_value
 
-def is_equal(int_list1, int_list2) -> bool:
+
+def is_equal(int_list1: list[int], int_list2: list[int]) -> bool:
+    """Check for deep equality between two lists."""
     if len(int_list1) != len(int_list2):
         return False
 
